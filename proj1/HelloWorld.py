def84c73234d0ec235db695e267d4ce5ed2e10ad
Tercio Almeida <tercio.oliveira.de.almeida@gmail.com>
2019-10-10 20:46:59 -0300

    Brincando com strings

--- proj1/HelloWorld.py
+++ proj1/HelloWorld.py
@@ -1,8 +1,18 @@
 __author__ = 'tércião'
 
+name = input('Please, enter your name: ')
+
 for i in range(10):
     greeting = 'Hello'
-    name = 'World'
     # this is a comment
     print(greeting + ' ' + name, str(i))
 
+print("String over\n\tmultiple lines\n\t\tthis is so cool")
+
+strangeString = """String
+    split over
+        several lines"""
+
+print(strangeString)
+
+print("""Olha só que interessante, consigo usar aspas " ' " no meio sem problemas """)
