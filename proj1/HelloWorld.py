e754d08fb8883311e5590d03de55d0a2f7c66dec
Tercio Almeida <tercio.oliveira.de.almeida@gmail.com>
2019-10-29 21:46:23 -0300

    muitas bagunças

--- proj1/HelloWorld.py
+++ proj1/HelloWorld.py
@@ -1,6 +1,6 @@
 __author__ = 'tércião'
 
-name = input('Please, enter your name: ')
+name = __author__ #input('Please, enter your name: ')
 
 for i in range(10):
     greeting = 'Hello'
@@ -15,4 +15,29 @@ strangeString = """String
 
 print(strangeString)
 
-print("""Olha só que interessante, consigo usar aspas " ' " no meio sem problemas """)
+tripleAspas = """Olha só que interessante, consigo usar aspas "_'_" no meio sem problemas"""
+
+print(tripleAspas)
+
+print(tripleAspas[:6])
+print(tripleAspas[6:])
+
+print(tripleAspas[-4:-2])
+
+print(tripleAspas[0::2])
+
+
+numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+print(numbers[0::3])
+
+print("eita " * 10)
+
+print("A Mariana tem " + str(5+2) + " anos!")
+
+print("A Mariana tem {0} anos de idade".format(5+2))
+
+print("A Mariana tem %d anos de idade e é uma pessoa muito %s" % (7, "legal"))
+
+print("""Era {0} {2} 
+{1} 
+{0}""".format("uma", "eita", "vez"))
